db9875424a4ae7b13b0263d39f2f760e67771ae7
Felix Chantrel <felchantrel@gmail.com>
2020-06-01 23:49:23 +0200

    First design of dashboard done.

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+const DEFAULT_CURRENCIES: Array<string> = ['USD', 'EUR', 'YEN', 'AUS', 'SOO', 'TOO', 'BOO', 'LOO'];
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +10,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardPage implements OnInit {
 
-  constructor() { }
+  private sellBtc: Boolean;
+  private currencies: Array<string>;
+
+  constructor(public alertController: AlertController) {
+    this.sellBtc = true;
+    this.currencies = JSON.parse(JSON.stringify(DEFAULT_CURRENCIES));
+  }
 
   ngOnInit() {
+
+  }
+
+  async addCurrency() {
+    let inputs = [];
+
+    for (let currency of DEFAULT_CURRENCIES.filter((c) => this.currencies.indexOf(c) === -1)) {
+      inputs.push({
+        name: currency,
+        type: 'checkbox',
+        label: currency,
+        value: currency,
+        checked: false
+      });
+    }
+
+    let alert: any;
+
+    if (inputs.length > 0) {
+      alert = await this.alertController.create({
+        header: 'Add currencies',
+        inputs: inputs,
+        buttons: [
+          {
+            text: 'Cancel',
+            role: 'cancel',
+          }, {
+            text: 'Add',
+            handler: (newCurrencies) => {
+              for (let newCurrency of newCurrencies) {
+                this.currencies.push(newCurrency);
+              }
+            }
+          }
+        ]
+      });
+
+    } else {
+      alert = await this.alertController.create({
+        header: 'Add currencies',
+        message: 'All currencies are already displayed',
+        buttons: ['OK']
+      });
+    }
+
+    await alert.present();
+  }
+
+  deleteCurrency(index: number) {
+    this.currencies.splice(index, 1);
+  }
+
+  swap() {
+    this.sellBtc = !this.sellBtc;
   }
 
 }
